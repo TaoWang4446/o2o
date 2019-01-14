28e6ce3094d829ea63991e165a4129b9dcbecc9a
TaoWang4446 <2825586682@qq.com>
2019-01-14 17:18:23 +0800

    根据id查询店铺实现

--- o2o/src/main/java/com/imooc/o2o/dao/ShopDao.java
+++ o2o/src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -5,14 +5,24 @@ import com.imooc.o2o.entity.Shop;
 public interface ShopDao {
 
 	/**
+	 * 通过shop id 查询店铺
+	 * 
+	 * @param shopId
+	 * @return shop
+	 */
+	Shop queryByShopId(long shopId);
+
+	/**
 	 * 新增店铺
+	 * 
 	 * @param shop
 	 * @return
 	 */
 	int insertShop(Shop shop);
-	
+
 	/**
 	 * 更新店铺信息
+	 * 
 	 * @param shop
 	 * @return
 	 */
--- o2o/src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
+++ o2o/src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
@@ -21,20 +21,29 @@ public class ShopDaoTest extends BaseTest {
 	private ShopDao shopDao;
 
 	@Test
+	//@Ignore
+	public void testQueryByShopId() {
+		long shopId = 1;
+		Shop shop = shopDao.queryByShopId(shopId);
+		System.out.println("areaId:" + shop.getArea().getAreaId());
+		System.out.println("areaName" + shop.getArea().getAreaName());
+	}
+
+	@Test
 	@Ignore
 	public void testInsertShop() {
 		Shop shop = new Shop();
 		PersonInfo owner = new PersonInfo();
 		Area area = new Area();
 		ShopCategory shopCategory = new ShopCategory();
-		
+
 		owner.setUserId(1L);
 		area.setAreaId(2);
 		shopCategory.setShopCategoryId(1L);
 		shop.setOwner(owner);
 		shop.setArea(area);
 		shop.setShopCategory(shopCategory);
-		
+
 		shop.setShopName("测试的店铺");
 		shop.setShopDesc("test");
 		shop.setShopAddr("test");
@@ -43,20 +52,20 @@ public class ShopDaoTest extends BaseTest {
 		shop.setCreateTime(new Date());
 		shop.setEnableStatus(1);
 		shop.setAdvice("审核中");
-		
+
 		int effectNum = shopDao.insertShop(shop);
 		assertEquals(1, effectNum);
 	}
-	
+
 	@Test
 	public void testUpdateShop() {
 		Shop shop = new Shop();
 		shop.setShopId(1L);
-		
+
 		shop.setShopDesc("测试描述");
 		shop.setShopAddr("测试地址");
 		shop.setLastEditTime(new Date());
-		
+
 		int effectNum = shopDao.updateShop(shop);
 		assertEquals(1, effectNum);
 	}
